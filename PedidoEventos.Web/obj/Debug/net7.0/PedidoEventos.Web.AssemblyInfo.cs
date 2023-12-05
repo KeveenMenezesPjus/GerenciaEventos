87bc9a5a6d1c9a8dd9d47da95a87b67667eab51e
Keveen Menezes <endykeven@gmail.com>
2023-12-05 19:55:22 -0300

    commit

--- PedidoEventos.Web/obj/Debug/net7.0/PedidoEventos.Web.AssemblyInfo.cs
+++ PedidoEventos.Web/obj/Debug/net7.0/PedidoEventos.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PedidoEventos.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7931034c976f8e71cd2b3af2025de07c66557f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PedidoEventos.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PedidoEventos.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
